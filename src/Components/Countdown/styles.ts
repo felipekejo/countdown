9c680f3dd8feb3fc8f0fce34f7040f822cf73f82
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2022-04-11 00:14:32 +0930

    more photos

--- src/Components/Countdown/styles.ts
+++ src/Components/Countdown/styles.ts
@@ -3,12 +3,14 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 display:grid;
-row-gap:400px;
+row-gap:350px;
 align-items: center;
 justify-content:center;
 h1{
+  
   text-align: center;
-  font-size:48px
+  font-size:48px;
+  color:#ffff00
 }
 `
 
--- src/utils/listLogos.ts
+++ src/utils/listLogos.ts
@@ -1,8 +1,13 @@
 import Casamento from '../../public/casamento.jpg'
+import Disney from '../../public/disney.jpg'
+import Fuji from '../../public/fuji.jpg'
+import Melbourne from '../../public/melbourne.jpg'
 import Mercadao from '../../public/mercadao.jpg'
 import NSW from '../../public/nsw.jpg'
 import Okinawa from '../../public/okinawa.jpg'
 import OperaHouse from '../../public/operahouse.jpg'
+import SA from '../../public/sa.jpg'
+
 
 export const listLogos = [
   {
@@ -20,5 +25,17 @@ export const listLogos = [
   },
   {
     img: NSW
+  },
+  {
+    img: Disney
+  },
+  {
+    img: Fuji
+  },
+  {
+    img: Melbourne
+  },
+  {
+    img: SA
   },
 ]
